bd5f6ff81b4e1f0297fec98e05fa864561475f32
Arnaud Thimel <thimel@codelutin.com>
2019-11-20 10:57:30 +0100

    Clean init script

--- fishola-rest/src/main/resources/db/migration/V1.0.0__initialize_Database.sql
+++ fishola-rest/src/main/resources/db/migration/V1.0.0__initialize_Database.sql
@@ -1,87 +1,89 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE gender
-  AS ENUM('Male', 'Female', 'NonBinary');
+    AS ENUM('Male', 'Female', 'NonBinary');
 
-create table fishola_user (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    first_name text NOT NULL,
-    last_name text,
-    email text NOT NULL UNIQUE,
-    password text NOT NULL,
+CREATE TABLE fishola_user (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
     gender gender,
-    birth_year int
+    birth_year INT
 );
 
-create table lake (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    name text NOT NULL UNIQUE
+CREATE TABLE lake (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
-create table weather (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    name text NOT NULL UNIQUE
+CREATE TABLE weather (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
-create table species (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    name text NOT NULL UNIQUE,
-    buildt_in boolean NOT NULL
+CREATE TABLE species (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    buildt_in BOOLEAN NOT NULL
 );
 
-create table method (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    name text NOT NULL UNIQUE,
-    buildt_in boolean NOT NULL
+CREATE TABLE method (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    buildt_in BOOLEAN NOT NULL
 );
 
-create table released_fish_state (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    name text NOT NULL UNIQUE
+CREATE TABLE released_fish_state (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
 );
 
 CREATE TYPE trip_type
-  AS ENUM('Border', 'Craft');
+    AS ENUM('Border', 'Craft');
 
-create table trip (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    owner uuid REFERENCES fishola_user(id),
-    live boolean NOT NULL,
-    name text NOT NULL,
-    day date NOT NULL,
-    start_time timestamp without time zone NOT NULL,
-    end_time timestamp without time zone NOT NULL,
+CREATE TABLE trip (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    owner UUID REFERENCES fishola_user(id),
+    live BOOLEAN NOT NULL,
+    name TEXT NOT NULL,
+    day DATE NOT NULL,
+    start_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    end_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     type trip_type NOT NULL,
-    latitude double precision NOT NULL,
-    longitude double precision NOT NULL,
-    lake uuid REFERENCES lake(id) NOT NULL,
-    weather uuid REFERENCES weather(id) NOT NULL
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    lake UUID REFERENCES lake(id) NOT NULL,
+    weather UUID REFERENCES weather(id) NOT NULL
 );
 
-create table trip_expected_species (
-    trip uuid REFERENCES trip(id) NOT NULL,
-    species uuid REFERENCES species(id) NOT NULL
+CREATE TABLE trip_expected_species (
+    trip UUID REFERENCES trip(id) NOT NULL,
+    species UUID REFERENCES species(id) NOT NULL
 );
 
-CREATE UNIQUE INDEX trip_expected_species_idx on trip_expected_species(trip, species);
+CREATE UNIQUE INDEX trip_expected_species_idx
+    ON trip_expected_species(trip, species);
 
-create table trip_methods (
-    trip uuid REFERENCES trip(id) NOT NULL,
-    method uuid REFERENCES method(id) NOT NULL
+CREATE TABLE trip_methods (
+    trip UUID REFERENCES trip(id) NOT NULL,
+    method UUID REFERENCES method(id) NOT NULL
 );
 
-CREATE UNIQUE INDEX trip_methods_idx on trip_methods(trip, method);
+CREATE UNIQUE INDEX trip_methods_idx
+    ON trip_methods(trip, method);
 
-create table catch (
-    id uuid DEFAULT uuid_generate_v4 () not null primary key,
-    trip uuid REFERENCES trip(id) NOT NULL,
-    catch_time timestamp without time zone NOT NULL,
-    species uuid REFERENCES species(id) NOT NULL,
-    method uuid REFERENCES method(id) NOT NULL,
-    picture oid,
-    size double precision,
-    weight double precision,
-    kept boolean NOT NULL,
-    released_fish_state uuid REFERENCES released_fish_state(id),
-    description text
+CREATE TABLE catch (
+    id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    trip UUID REFERENCES trip(id) NOT NULL,
+    catch_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    species UUID REFERENCES species(id) NOT NULL,
+    method UUID REFERENCES method(id) NOT NULL,
+    picture OID,
+    size DOUBLE PRECISION,
+    weight DOUBLE PRECISION,
+    kept BOOLEAN NOT NULL,
+    released_fish_state UUID REFERENCES released_fish_state(id),
+    description TEXT
 );
