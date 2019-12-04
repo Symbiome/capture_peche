b516b59340b6c468f5136dca703e1048d42e4cc4
Arnaud Thimel <thimel@codelutin.com>
2019-12-04 15:29:38 +0100

    Affichage de la liste si pas de sorties

--- fishola-mobile/src/components/my-trips/MyTripsList.vue
+++ fishola-mobile/src/components/my-trips/MyTripsList.vue
@@ -3,6 +3,16 @@
     <div v-for="t in trips" v-bind:key="t.id">
       <MyTripsItem v-bind:trip="t"/>
     </div>
+    <div v-if="trips.length == 0" class="no-trips">
+      <div class="top">
+        <img src="/img/illustration_fish.svg"/>
+        <span>Aucune sortie de pêche</span>
+      </div>
+      <div class="bottom">
+        <span>Commencez votre <br/>première sortie !</span>
+        <i class="icon-triangle"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,14 +47,51 @@ export default class MyTripsList extends Vue {
   border-top-left-radius: 30px;
   border-top-right-radius: 30px;
 
-  p {
-    margin: 0px;
-    padding-left: 30px;
-    border-bottom: 1px solid @gainsboro;
-  }
+  .no-trips {
+    height: calc(100vh - 274px);
+    background-color: @white-smoke;
+    // border: 1px solid red;
 
-  hr { 
-    color: @gainsboro;
+    display: flex;
+    flex-direction: column;
+
+        justify-content: space-between;
+
+
+    .top {
+      height: calc(100% - 176px);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      span {
+        font-size: 18px;
+        line-height: 25px;
+        color: @pale-sky;
+        text-align: center;
+        margin-top: 30px;
+      }
+    }
+
+    .bottom {
+      height: 176px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      text-align: center;
+
+      span {
+        color: @pelorous;
+        font-size: 18px;
+        line-height: 25px;
+      }
+      i {
+        color: @terra-cotta;
+        font-size: 12px;
+        margin-top: 10px;
+      }
+    }
   }
 }
 
