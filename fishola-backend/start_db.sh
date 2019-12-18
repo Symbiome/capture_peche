1c571f1fa0441eb2ffe8fd08545040fa1a38f3f4
Arnaud Thimel <thimel@codelutin.com>
2019-12-18 15:09:53 +0100

    Passage à PostgreSQL 12

--- fishola-backend/start_db.sh
+++ fishola-backend/start_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DBdir=/home/postgresql-11
+DBdir=/home/postgresql-12
 DB=fishola
 docker run \
-  --name postgres-11-${DB} \
+  --name postgres-12-${DB} \
   --restart always \
   -v ${DBdir}/${DB}:/var/lib/postgresql/data \
   -e POSTGRES_DB=${DB} \
-  -d postgres:11
+  -d postgres:12
