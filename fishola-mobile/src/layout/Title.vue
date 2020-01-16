375983ec62d9f696abfce36ba6b54963190e71b2
Arnaud Thimel <thimel@codelutin.com>
2020-01-16 12:12:57 +0100

    Affichage de l'environnement

--- fishola-mobile/src/layout/Title.vue
+++ fishola-mobile/src/layout/Title.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header-title">
     <img src="img/logo-small.svg" alt="Fishola" />
+    <span v-if="envName" class="env">({{envName}})</span>
     <!--
     <img src="img/logo-FISHOLA-logo-negatif.svg" alt="Fishola" />
     <img src="img/logo-FISHOLA-nom-negatif.svg" alt="Fishola" />
@@ -13,17 +14,27 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Title extends Vue {
+
+  envName?:string = process.env.VUE_APP_ENV;
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 
+@import "../less/main";
+
 .header-title {
   font-size: 24px;
 
   img {
     height: 30px;
+  }
+
+  span.env {
+    color: @terra-cotta;
+    font-size: 16px;
   }
 }
 </style>
