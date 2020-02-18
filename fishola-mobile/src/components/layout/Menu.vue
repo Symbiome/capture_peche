8f58da32088280f32da2a9613209db4386ee2796
Arnaud Thimel <thimel@codelutin.com>
2020-02-18 20:10:38 +0100

    Mise à jour du style du menu

--- fishola-mobile/src/components/layout/Menu.vue
+++ fishola-mobile/src/components/layout/Menu.vue
@@ -2,60 +2,60 @@
   <div class="menu" v-bind:class="visibility">
       <div class="close"
            v-on:click="closeMenu">
-        +
+           <div class="plus">+</div> 
       </div>
       <div class="items">
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Profil (TODO)
           </span>
           <i class="icon-profile"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Accueil
           </span>
           <i class="icon-home"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Tableau de bord
           </span>
           <i class="icon-dashboard"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Paramètres
           </span>
           <i class="icon-settings"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Documentation
           </span>
-          <i class="icon-file"/>
+          <i class="icon-files"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Infos / Crédits
           </span>
           <i class="icon-info"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Des retours ?
           </span>
           <i class="icon-faq"/>
         </div>
 
-        <div class="item">
+        <div class="item" v-on:click="wip">
           <span>
             Déconnexion
           </span>
@@ -93,6 +93,11 @@ export default class Menu extends Vue {
   beforeDestroy() {
     console.log("Destroy menu");
     this.$root.$off('open-menu');
+  }
+
+  wip() {
+    // this.closeMenu();
+    this.$root.$emit('toaster-warning', 'Work in progress');
   }
 
 }
@@ -140,7 +145,6 @@ export default class Menu extends Vue {
 
     display: flex;
     flex-direction: column;
-    // justify-content: center;
     align-items: flex-end;
 
     color:white;
@@ -148,9 +152,19 @@ export default class Menu extends Vue {
     padding: 15px;
 
     .close {
-      font-size: 30px;
-      transform: rotate(45deg);
+      display: flex;
+      flex-direction: row-reverse;
+      align-items: center;
+
+      height: 30px;
       margin: 10px;
+      width: 100%;
+
+      div.plus {
+        font-size: 30px;
+        width: fit-content;
+        transform: rotate(45deg);
+      }
     }
 
     .items {
