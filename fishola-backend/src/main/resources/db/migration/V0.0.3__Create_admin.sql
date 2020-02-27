73e0fc891039d97b531d21e94aac708d02b7d431
Arnaud Thimel <thimel@codelutin.com>
2020-02-27 14:41:04 +0100

    Ajoute un compte admin pour Chloé

--- fishola-backend/src/main/resources/db/migration/V0.0.3__Create_admin.sql
+++ fishola-backend/src/main/resources/db/migration/V0.0.3__Create_admin.sql
@@ -1,2 +1,4 @@
 insert into fishola_user (first_name, last_name, email, password) values
     ('Arnaud', 'Thimel', 'thimel@codelutin.com', '$2a$12$NSO7uY1VZwBeEc08FTtdUef3OpPQv0lDDZwRLHcPvl2dOtNDZ8OBu');
+insert into fishola_user (first_name, last_name, email, password) values
+    ('Chloé', 'Goulon', 'chloe.goulon@inrae.fr', '$2a$12$3NOX6pT.yf5eZH9rUXSkxOr.7.afF2nC52C4iMf.HFg6YV9VgeM2i');
