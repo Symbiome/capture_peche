017e30154cd7dcd4c2ad1cd836946ff8178b3de6
Arnaud Thimel <thimel@codelutin.com>
2020-04-20 12:26:44 +0200

    Mise en place d'une vraie gestion de config

--- fishola-backend/src/main/java/fr/inra/fishola/FisholaConfiguration.java
+++ fishola-backend/src/main/java/fr/inra/fishola/FisholaConfiguration.java
@@ -1,67 +1,66 @@
 package fr.inra.fishola;
 
 import fr.inra.fishola.exceptions.FisholaTechnicalException;
-import org.apache.commons.lang3.StringUtils;
+import io.quarkus.arc.config.ConfigProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.inject.Singleton;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.util.Optional;
 import java.util.Properties;
 
-@Singleton
-public class FisholaConfiguration {
+@ConfigProperties(prefix = "fishola")
+public interface FisholaConfiguration {
 
-    private static final Log log = LogFactory.getLog(FisholaConfiguration.class);
+    @ConfigProperty(defaultValue = "12")
+    int getPasswordHashCost();
 
-    public int getPasswordHashCost() {
-        return 12;
-    }
+    String getJwtSecret();
 
-    public String getJwtSecret() {
-        return "v3ry 53cr37 k3y";
-    }
+    @ConfigProperty(defaultValue = "148")
+    int getTripModifiableHours();
 
-    public String getBackendBaseUrl() {
-        return null; // "https://fishola.demo.codelutin.com"
-    }
+    Optional<String> getBackendBaseUrl();
 
-    public int getTripModifiableHours() {
-//        return 7 * 24; // 7 jours
-        return 24; // 1 jour
-//        return 5; // 5 heures
-    }
-
-    public String getBackendBaseUrl(HttpServletRequest httpServletRequest) {
-        String backendBaseUrl = getBackendBaseUrl();
-        if (StringUtils.isEmpty(backendBaseUrl)) {
-            String requestUrl = httpServletRequest.getRequestURL().toString();
-            String requestUri = httpServletRequest.getRequestURI();
-            int index = requestUrl.indexOf(requestUri);
-            if (index != -1) {
-                backendBaseUrl = requestUrl.substring(0, index);
-                if (log.isDebugEnabled()) {
-                    log.debug(String.format("No 'BackendBaseUrl' in conf, computed is: %s", backendBaseUrl));
-                }
-            } else {
-                throw new FisholaTechnicalException("Unable to compute backendBaseUrl", null);
-            }
+    default String getBackendBaseUrl(HttpServletRequest httpServletRequest) {
+        Optional<String> backendBaseUrl = getBackendBaseUrl();
+        String result;
+        if (backendBaseUrl.isPresent()) { // Ne pas utiliser de lambda sinon ça plante au démarrage
+            result = backendBaseUrl.get();
+        } else {
+            result = computeBackendBaseUrl(httpServletRequest);
         }
-        return backendBaseUrl;
+        return result;
     }
 
-    public String getApiUrl(String path, HttpServletRequest httpServletRequest) {
+    default String computeBackendBaseUrl(HttpServletRequest httpServletRequest) {
+        String requestUrl = httpServletRequest.getRequestURL().toString();
+        String requestUri = httpServletRequest.getRequestURI();
+        int index = requestUrl.indexOf(requestUri);
+        if (index != -1) {
+            String guessed = requestUrl.substring(0, index);
+            Log log = LogFactory.getLog(FisholaConfiguration.class);
+            if (log.isDebugEnabled()) {
+                log.debug(String.format("No 'BackendBaseUrl' in conf, computed is: %s", guessed));
+            }
+            return guessed;
+        } else {
+            throw new FisholaTechnicalException("Unable to compute backendBaseUrl", null);
+        }
+    }
+
+    default String getApiUrl(String path, HttpServletRequest httpServletRequest) {
         String backendBaseUrl = getBackendBaseUrl(httpServletRequest);
         String result = String.format("%s%s", backendBaseUrl, path);
         return result;
     }
 
-    public String getMailFrom() {
-        return "fishola@codelutin.com";
-    }
+    @ConfigProperty(defaultValue = "fishola@codelutin.com")
+    String getMailFrom();
 
-    public Properties getMailProperties() {
+    default Properties getMailProperties() {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", false);
         prop.put("mail.smtp.starttls.enable", "false");
@@ -71,17 +70,20 @@ public class FisholaConfiguration {
         return prop;
     }
 
-    public float getRawImageQuality() {
-        // Par défaut la qualité semble être de ~0.74.
-        // Pour une qualité équivalent mettre 0.98.
-        // .90 pourrait être un bon compromis.
+    /**
+     * Par défaut la qualité semble être de ~0.74.
+     * Pour une qualité équivalent mettre 0.98.
+     * .90 pourrait être un bon compromis.
+     */
+    @ConfigProperty(defaultValue = ".90f")
+    float getRawImageQuality();
 
-//        return .98f;
-        return .90f;
-    }
+    @ConfigProperty(defaultValue = "/tmp/fishola-pictures")
+    String getPicturesPreviewFolderPath();
 
-    public File getPicturesPreviewFolder() {
-        File result = new File("/tmp/fishola-pictures");
+    default File getPicturesPreviewFolder() {
+        File result = new File(getPicturesPreviewFolderPath());
+        Log log = LogFactory.getLog(FisholaConfiguration.class);
         if (result.mkdirs() && log.isInfoEnabled()) {
             log.info("Création du dossier de stockage des previews : " + result.getAbsolutePath());
         }
