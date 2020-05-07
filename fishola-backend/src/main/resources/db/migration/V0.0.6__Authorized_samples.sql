27753285b805338b393787b75ce3ea58922dc884
Arnaud Thimel <thimel@codelutin.com>
2020-05-07 15:44:38 +0200

    Finalement les 3 poissons sont prélevés sur chaque lac

--- fishola-backend/src/main/resources/db/migration/V0.0.6__Authorized_samples.sql
+++ fishola-backend/src/main/resources/db/migration/V0.0.6__Authorized_samples.sql
@@ -1,13 +1,6 @@
 
--- Corégone et Truite sur tous les lacs
+-- Corégone, Truite & Omble chevalier sur tous les lacs
 INSERT INTO authorized_sample (lake_id, species_id)
     SELECT l.id AS lake_id, s.id AS species_id
     FROM lake l, species s
-    WHERE s.name in ('Corégone', 'Truite');
-
--- Omble uniquement sur Léman et Annecy
-INSERT INTO authorized_sample (lake_id, species_id)
-    SELECT l.id AS lake_id, s.id AS species_id
-    FROM lake l, species s
-    WHERE s.name = 'Omble chevalier'
-    AND l.name in ('Lac d''Annecy', 'Léman');
+    WHERE s.name in ('Corégone', 'Truite', 'Omble chevalier');
