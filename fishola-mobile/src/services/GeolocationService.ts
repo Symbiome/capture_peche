d8d66882cf89197c04f9aaf37db14a27074ab416
Arnaud Thimel <thimel@codelutin.com>
2020-05-19 15:23:22 +0200

    Utilise l'API de capacitor pour l'acquisition de la position GPS

--- fishola-mobile/src/services/GeolocationService.ts
+++ fishola-mobile/src/services/GeolocationService.ts
@@ -2,6 +2,9 @@ import {Lake} from '@/pojos/BackendPojos';
 import AbstractFisholaService from '@/services/AbstractFisholaService';
 import ReferentialService from '@/services/ReferentialService';
 
+import { Plugins, GeolocationPosition } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
 export class CoordsAndLake {
 
     constructor (
@@ -19,27 +22,101 @@ export default class GeolocationService extends AbstractFisholaService {
         super();
     }
 
-    static getPosition():Promise<any> {
-        if (!navigator.geolocation) {
-            return Promise.reject("Geoloc non supportée par le navigateur");
-        }
+    static getPosition():Promise<GeolocationPosition> {
+
         return new Promise<any>((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition(resolve, reject);
+
+            console.log("Before getCurrentPosition", new Date());
+
+            let positionPromise:Promise<GeolocationPosition> = Geolocation.getCurrentPosition({
+                enableHighAccuracy: false,
+                maximumAge: 20,
+                timeout: 10000
+            });
+            positionPromise.then(
+                (position:GeolocationPosition) => {
+
+                    console.log("Got currentPosition", new Date());
+                    console.log("position", JSON.stringify(position));
+                    resolve(position);
+                    /* enableHighAccuracy: true
+                    {
+                        "coords":{
+                            "latitude":47.1552435,
+                            "longitude":-1.5262437,
+                            "accuracy":21.48699951171875,
+                            "altitude":77.79999542236328,
+                            "altitudeAccuracy":2,
+                            "speed":0.0025068377144634724,
+                            "heading":315.3136291503906
+                        },
+                        "timestamp":1589819559643
+                    }
+
+                    */
+
+                    /* enableHighAccuracy: false
+                    {
+                        "coords":{
+                            "latitude":47.1552424,
+                            "longitude":-1.5262452,
+                            "accuracy":21.347000122070312,
+                            "altitude":77.79999542236328,
+                            "altitudeAccuracy":2,
+                            "speed":0.012332512997090816,
+                            "heading":274.6920471191406
+                        },
+                        "timestamp":1589819653984
+                    }
+                    */
+                },
+                (failure) => {
+                    console.error("Unable to get current position", failure);
+                    reject(failure);
+                })
+
+            // if (!navigator.geolocation) {
+            //     return Promise.reject("Geoloc non supportée par le navigateur");
+            // }
+            // return new Promise<any>((resolve, reject) => {
+            //     navigator.geolocation.getCurrentPosition(resolve, reject);
+            // });
+
+            /* De base :
+
+                GeolocationPosition {
+                    coords: { (GeolocationCoordinates)
+                        accuracy: 22.951000213623047
+                        altitude: 77.5
+                        altitudeAccuracy: null
+                        heading: 263.546142578125
+                        latitude: 47.1552407
+                        longitude: -1.5262464
+                        speed: 0.0011239566374570131
+                    }
+                    timestamp: 1589884798399 (number)
+                }
+
+            */
         });
+
     }
 
     static getClosestLake():Promise<CoordsAndLake> {
         return new Promise<CoordsAndLake>((resolve, reject) => {
-            let promises = [ReferentialService.getLakes(), GeolocationService.getPosition()];
-            Promise.all(promises)
+            Promise.all(
+                [
+                    ReferentialService.getLakes(),
+                    GeolocationService.getPosition()
+                ])
                 .then(
-                    (data:any[]) => {
+                    (data:[Lake[], GeolocationPosition]) => {
                         let lakes:Lake[] = data[0];
 
                         if (!lakes || lakes.length == 0) {
                             reject();
                         } else {
-                            let position = data[1];
+                            let position:GeolocationPosition = data[1];
 
                             console.log("lakes", lakes);
                             console.log("position", position);
