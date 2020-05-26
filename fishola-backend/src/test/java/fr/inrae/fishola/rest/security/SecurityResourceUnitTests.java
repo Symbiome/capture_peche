ab3cbff8db85d8d7cfd84559099ce6d1155a2896
Arnaud Thimel <thimel@codelutin.com>
2020-05-26 15:06:09 +0200

    [iso] Imports static pour alléger le code

--- fishola-backend/src/test/java/fr/inrae/fishola/rest/security/SecurityResourceUnitTests.java
+++ fishola-backend/src/test/java/fr/inrae/fishola/rest/security/SecurityResourceUnitTests.java
@@ -6,53 +6,55 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
 
+import static fr.inrae.fishola.rest.security.SecurityResource.encodeSampleBaseId;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SecurityResourceUnitTests {
 
     @Test
     public void testEncodeSampleBaseId() {
-        Assertions.assertEquals("A", SecurityResource.encodeSampleBaseId(0));
-        Assertions.assertEquals("B", SecurityResource.encodeSampleBaseId(1));
-        Assertions.assertEquals("D", SecurityResource.encodeSampleBaseId(3));
-        Assertions.assertEquals("Z", SecurityResource.encodeSampleBaseId(25));
-        Assertions.assertEquals("AA", SecurityResource.encodeSampleBaseId(26));
-        Assertions.assertEquals("AB", SecurityResource.encodeSampleBaseId(27));
-        Assertions.assertEquals("AC", SecurityResource.encodeSampleBaseId(28));
-        Assertions.assertEquals("AD", SecurityResource.encodeSampleBaseId(29));
-        Assertions.assertEquals("AE", SecurityResource.encodeSampleBaseId(30));
-        Assertions.assertEquals("AF", SecurityResource.encodeSampleBaseId(31));
-        Assertions.assertEquals("AG", SecurityResource.encodeSampleBaseId(32));
-        Assertions.assertEquals("AH", SecurityResource.encodeSampleBaseId(33));
-        Assertions.assertEquals("AI", SecurityResource.encodeSampleBaseId(34));
-        Assertions.assertEquals("AJ", SecurityResource.encodeSampleBaseId(35));
-        Assertions.assertEquals("AK", SecurityResource.encodeSampleBaseId(36));
-        Assertions.assertEquals("AL", SecurityResource.encodeSampleBaseId(37));
-        Assertions.assertEquals("AM", SecurityResource.encodeSampleBaseId(38));
-        Assertions.assertEquals("AN", SecurityResource.encodeSampleBaseId(39));
-        Assertions.assertEquals("AO", SecurityResource.encodeSampleBaseId(40));
-        Assertions.assertEquals("AP", SecurityResource.encodeSampleBaseId(41));
-        Assertions.assertEquals("AQ", SecurityResource.encodeSampleBaseId(42));
-        Assertions.assertEquals("AR", SecurityResource.encodeSampleBaseId(43));
-        Assertions.assertEquals("AS", SecurityResource.encodeSampleBaseId(44));
-        Assertions.assertEquals("AT", SecurityResource.encodeSampleBaseId(45));
-        Assertions.assertEquals("AU", SecurityResource.encodeSampleBaseId(46));
-        Assertions.assertEquals("AV", SecurityResource.encodeSampleBaseId(47));
-        Assertions.assertEquals("AW", SecurityResource.encodeSampleBaseId(48));
-        Assertions.assertEquals("AX", SecurityResource.encodeSampleBaseId(49));
-        Assertions.assertEquals("AY", SecurityResource.encodeSampleBaseId(50));
-        Assertions.assertEquals("AZ", SecurityResource.encodeSampleBaseId(51));
-        Assertions.assertEquals("BA", SecurityResource.encodeSampleBaseId(52));
-        Assertions.assertEquals("GH", SecurityResource.encodeSampleBaseId(189));
-        Assertions.assertEquals("BALEC", SecurityResource.encodeSampleBaseId(939772));
+        assertEquals("A", encodeSampleBaseId(0));
+        assertEquals("B", encodeSampleBaseId(1));
+        assertEquals("D", encodeSampleBaseId(3));
+        assertEquals("Z", encodeSampleBaseId(25));
+        assertEquals("AA", encodeSampleBaseId(26));
+        assertEquals("AB", encodeSampleBaseId(27));
+        assertEquals("AC", encodeSampleBaseId(28));
+        assertEquals("AD", encodeSampleBaseId(29));
+        assertEquals("AE", encodeSampleBaseId(30));
+        assertEquals("AF", encodeSampleBaseId(31));
+        assertEquals("AG", encodeSampleBaseId(32));
+        assertEquals("AH", encodeSampleBaseId(33));
+        assertEquals("AI", encodeSampleBaseId(34));
+        assertEquals("AJ", encodeSampleBaseId(35));
+        assertEquals("AK", encodeSampleBaseId(36));
+        assertEquals("AL", encodeSampleBaseId(37));
+        assertEquals("AM", encodeSampleBaseId(38));
+        assertEquals("AN", encodeSampleBaseId(39));
+        assertEquals("AO", encodeSampleBaseId(40));
+        assertEquals("AP", encodeSampleBaseId(41));
+        assertEquals("AQ", encodeSampleBaseId(42));
+        assertEquals("AR", encodeSampleBaseId(43));
+        assertEquals("AS", encodeSampleBaseId(44));
+        assertEquals("AT", encodeSampleBaseId(45));
+        assertEquals("AU", encodeSampleBaseId(46));
+        assertEquals("AV", encodeSampleBaseId(47));
+        assertEquals("AW", encodeSampleBaseId(48));
+        assertEquals("AX", encodeSampleBaseId(49));
+        assertEquals("AY", encodeSampleBaseId(50));
+        assertEquals("AZ", encodeSampleBaseId(51));
+        assertEquals("BA", encodeSampleBaseId(52));
+        assertEquals("GH", encodeSampleBaseId(189));
+        assertEquals("BALEC", encodeSampleBaseId(939772));
     }
 
     @Test
     public void testEncodeSampleBaseIdNoConflict() {
         Set<String> done = new HashSet<>();
         for (int i=0; i<1000000; i++) {
-            String encoded = SecurityResource.encodeSampleBaseId(i);
+            String encoded = encodeSampleBaseId(i);
             Assertions.assertTrue(done.add(encoded));
         }
-
     }
 
 }
