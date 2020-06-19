008a358429129b3c01acb27104e7a86318c411d2
Arnaud Thimel <thimel@codelutin.com>
2020-06-19 18:49:23 +0200

    Mise à jour des headers des fichiers

--- fishola-backend/src/main/resources/db/migration/V0.0.5__Create_admin.sql
+++ fishola-backend/src/main/resources/db/migration/V0.0.5__Create_admin.sql
@@ -1,3 +1,23 @@
+---
+-- #%L
+-- Fishola :: Backend
+-- %%
+-- Copyright (C) 2019 - 2020 INRAE - UMR CARRTEL
+-- %%
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- #L%
+---
 insert into fishola_user (first_name, last_name, email, password) values
     ('Arnaud', 'Thimel', 'thimel@codelutin.com', '$2a$12$NSO7uY1VZwBeEc08FTtdUef3OpPQv0lDDZwRLHcPvl2dOtNDZ8OBu');
 insert into fishola_user (first_name, last_name, email, password) values
