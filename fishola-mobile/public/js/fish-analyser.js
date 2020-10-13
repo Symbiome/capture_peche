613e5b00319eb9a33c7018edf3874e90d2f12596
Alex Morel <amorel@codelutin.com>
2020-10-13 15:38:24 +0200

    [Fish measure] Contour detection using openCV

--- fishola-mobile/public/js/fish-analyser.js
+++ fishola-mobile/public/js/fish-analyser.js
@@ -30,18 +30,24 @@
         var erosion_type = cv.MorphShapes.MORPH_RECT.value;
         var erosion_size = [2*Control.erosion_size+1, 2*Control.erosion_size+1];
         var element = cv.getStructuringElement(erosion_type, erosion_size, [-1, -1]);
-        var dst = new cv.Mat();
-        cv.erode(output2, output1, element, [-1, -1], 1, cv.BORDER_CONSTANT, borderValue);
-        result = output1;*/
-        /*      let contours = new cv.MatVector();
+        cv.erode(output2, output1, element, [-1, -1], 1, cv.BORDER_CONSTANT, borderValue);*/
+       
+        // Step 3: find coutours in the edge map
+        cv.threshold(output2, output1, 120, 200, cv.THRESH_BINARY);
+        let dst = cv.Mat.zeros(output1.cols, output1.rows, cv.CV_8UC3);
+        let contours = new cv.MatVector();
         let hierarchy = new cv.Mat();
-        cv.findContours(output2, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+        // You can try more different parameters
+        cv.findContours(output1, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
         // draw contours with random Scalar
         for (let i = 0; i < contours.size(); ++i) {
             let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
                                     Math.round(Math.random() * 255));
-            cv.drawContours(output2, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-        }*/
+            cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+        }
+        cv.imshow('canvasOutput1', src);
+        cv.imshow('canvasOutput2', output1);
+        cv.imshow('canvasOutput3', dst);
         // find contours in the edge map
         //cnts = cv.findContours(edged.copy(), cv.RETR_EXTERNAL,
         //  cv.CHAIN_APPROX_SIMPLE);
@@ -50,7 +56,6 @@
         // 'pixels per metric' calibration variable
         //(cnts, _) = contours.sort_contours(cnts);
         //pixelsPerMetric = None;
-        cv.imshow('canvasOutput', output2);
         src.delete();
         dst.delete();
       };
