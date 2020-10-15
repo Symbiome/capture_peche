616dc3262ee6e8d4c67ade470e0ecd0cf51d37e7
Alex Morel <amorel@codelutin.com>
2020-10-15 17:17:36 +0200

    [Fish measure] Resizer picture (to improve performances)

--- fishola-mobile/public/js/fish-analyser.js
+++ fishola-mobile/public/js/fish-analyser.js
@@ -14,24 +14,29 @@
 
 
     function calculateSizes(imgElement, minSizeRatio, leftSizeObjectSizeMm) {
+        /*let empty = new cv.Mat();
+        cv.imshow('canvasOutput1', empty);
+        cv.imshow('canvasOutput2', empty);
+        cv.imshow('canvasOutput3', empty);*/
         // See https://github.com/ucisysarch/opencvjs/blob/master/test/img_proc.html
         // And https://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
-        // Step 1: load the image, convert it to grayscale, and blur it slightly
-        let src = cv.imread(imgElement);
+        // Step 1: load the image & resize it
+        let src = resize(imgElement);
+        //  Step 2 : convert it to grayscale, and blur it slightly
         let imgSize = Math.max(src.cols, src.rows);
         let refined1 = new cv.Mat();
         let refined2 = new cv.Mat();
         cv.cvtColor(src, refined1, cv.COLOR_BGR2GRAY, 0);
         let blurSize = new cv.Size(7, 7);
         cv.GaussianBlur(refined1, refined2, blurSize, 0, 0, cv.BORDER_DEFAULT);
-        // Step 2: perform edge detection, then perform a dilation + erosion to
+        // Step 3: perform edge detection, then perform a dilation + erosion to
         // close gaps in between object edges
-        // Step 2.1 : edge detection
+        // Step 3.1 : edge detection
         cv.Canny(refined2, refined1, 50, 100, 3, false);
-        // Step 2.2 : dilation
+        // Step 3.2 : dilation
         const kernel = cv.getStructuringElement(cv.MORPH_RECT,new cv.Size(5,5));
         cv.dilate(refined1, refined2, kernel, new cv.Point(-1, 1), 1);
-        // Step 2.3 : erosion
+        // Step 3.3 : erosion
        /* TODO
         var borderValue = cv.Scalar.all(Number.MAX_VALUE);
         var erosion_type = cv.MorphShapes.MORPH_RECT.value;
@@ -39,16 +44,16 @@
         var element = cv.getStructuringElement(erosion_type, erosion_size, [-1, -1]);
         cv.erode(refined2, refined1, element, [-1, -1], 1, cv.BORDER_CONSTANT, borderValue);*/
        
-        // Step 3: find coutours in the edge map
-        // Step 3.1 : find contours
+        // Step 4: find coutours in the edge map
+        // Step 4.1 : find contours
         cv.threshold(refined2, refined1, 120, 200, cv.THRESH_BINARY);
-        let dst = cv.imread(imgElement);
+        let dst = resize(imgElement);
         let contours = new cv.MatVector();
         let hierarchy = new cv.Mat();
         let poly = new cv.MatVector();
         cv.findContours(refined1, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
 
-        // Step 3.2 : approximates each contour to polygon
+        // Step 4.2 : approximates each contour to polygon
         for (let i = 0; i < contours.size(); ++i) {
             let tmp = new cv.Mat();
             let cnt = contours.get(i);
@@ -57,16 +62,14 @@
             cnt.delete(); tmp.delete();
         }
 
-        // Step 4: bounding boxes (min area rects)
-        // Only keep big rectancles
-        // Sort from left to right
+        // Step 5: bounding boxes (min area rects)
+        // Only keep "big" bounding boxes
         let results = [];
         for (let i = 0; i < poly.size(); ++i) {
             let color = new cv.Scalar(Math.round(Math.random() * 255), Math.round(Math.random() * 255),
                                     Math.round(Math.random() * 255));
             let cnt = poly.get(i);
             let rotatedRect = cv.minAreaRect(cnt);
-            console.log(rotatedRect.center);
             let vertices = cv.RotatedRect.points(rotatedRect);
             let rectangleSize = Math.max(rotatedRect.size.width, rotatedRect.size.height);
             // If rectangle width or height is at least minSizeRatio% of full image size
@@ -80,11 +83,11 @@
                 results.push({'left_x': minX, 'center_x': rotatedRect.center.x, 'center_y': rotatedRect.center.y, 'size': rectangleSize});
             }
         }
-        // Step 5: sort from left to right
+        // Step 6: sort from left to right
         let sorted = results.sort((a,b) =>  a.left_x - b.left_x);
         console.log(sorted);
 
-        // Step 6: compute sizes
+        // Step 7: compute sizes
         let ratio = -1;
         let html1 =  "<table style='margin-left:50px;border:1px solid black'><tr><td style='border:1px solid black'>Size in pixels</td>";
         let html2 =  "<tr><td style='border:1px solid black'>Size in mm </td>";
@@ -101,7 +104,7 @@
         };
         html = html1 + '</tr>' + html2 + '</tr></table>';
         document.getElementById('status').innerHTML = html;
-        cv.imshow('canvasOutput1', src);
+        cv.imshow('canvasOutput1', src);origin
         cv.imshow('canvasOutput2', refined1);
         cv.imshow('canvasOutput3', dst);
 
@@ -109,5 +112,25 @@
         refined1.delete();
         refined2.delete();
         dst.delete();
+    }
+
+    function resize(imgElement) {
+        let original = cv.imread(imgElement);
+        let src = new cv.Mat();
+        // Vertical images
+        if (original.cols < original.rows) {
+            // Fix with to 300
+            let reducedWidth = 300;
+            let ratioWith = (reducedWidth/ original.cols);
+            let dsize = new cv.Size(reducedWidth, ratioWith * original.rows);
+            cv.resize(original, src, dsize, 0, 0, cv.INTER_AREA);
+        } else {
+            // Fix height to 300
+            let reduceHeight = 300;
+            let ratioHeight = (reduceHeight/ original.rows);
+            let dsize = new cv.Size(ratioHeight * original.cols, reduceHeight);
+            cv.resize(original, src, dsize, 0, 0, cv.INTER_AREA);
+        }
+        return src;
     }
 
