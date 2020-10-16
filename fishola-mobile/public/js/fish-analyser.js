dc720d65641fedf08f6559e0b0e4dfbeee76bbe4
Alex Morel <amorel@codelutin.com>
2020-10-16 14:35:50 +0200

    [Fish measure] Marker recognition prototype

--- fishola-mobile/public/js/fish-analyser.js
+++ fishola-mobile/public/js/fish-analyser.js
@@ -1,61 +1,74 @@
 
     function onOpenCvReady() {
-    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+        document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+        setTimeout(() => {
+            calculateSizes(imgElement, document.getElementById('minCoverrage').value, 
+            document.getElementById('leftSizeObjectSizeMm').value, 
+            parseInt(document.getElementById('fixedSize').value),
+            document.getElementById('marker'));
+        }, 2000);
+   
     }
     
+
+    document.getElementById('markerFile').addEventListener('change', (e) => {
+        document.getElementById('marker').src = URL.createObjectURL(e.target.files[0]);
+    }, false);
+
     let imgElement = document.getElementById('imageSrc');
     let inputElement = document.getElementById('fileInput');
     inputElement.addEventListener('change', (e) => {
-    imgElement.src = URL.createObjectURL(e.target.files[0]);
+        imgElement.src = URL.createObjectURL(e.target.files[0]);
     }, false);
     imgElement.onload = function() {
         calculateSizes(imgElement, document.getElementById('minCoverrage').value, 
         document.getElementById('leftSizeObjectSizeMm').value, 
-        parseInt(document.getElementById('fixedSize').value));
+        parseInt(document.getElementById('fixedSize').value),
+        document.getElementById('marker'));
     };
-
-
-    function calculateSizes(imgElement, minSizeRatio, leftSizeObjectSizeMm, fixedSize) {
-        /*let empty = new cv.Mat();
-        cv.imshow('canvasOutput1', empty);
-        cv.imshow('canvasOutput2', empty);
-        cv.imshow('canvasOutput3', empty);*/
+    function calculateSizes(imgElement, minSizeRatio, leftSizeObjectSizeMm, fixedSize, imgMarker) {
         // See https://github.com/ucisysarch/opencvjs/blob/master/test/img_proc.html
         // And https://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
-        // Step 1: load the image & resize it
-        let src = resize(imgElement, fixedSize);
-        //  Step 2 : convert it to grayscale, and blur it slightly
+        // Step 1: load the images & resize them
+        let src = cv.imread(imgElement); //resize(imgElement, fixedSize);
+        let marker = cv.imread(imgMarker); //resize(imgMarker, fixedSize / 2);
+        let dst = cv.imread(imgElement); //resize(imgElement, fixedSize);
+
+        // Step 2: match template
+        let matchedDst = new cv.Mat();
+        let mask = new cv.Mat();
+        cv.matchTemplate(src, marker, matchedDst, cv.TM_CCOEFF, mask);
+        let result = cv.minMaxLoc(matchedDst, mask);
+        let maxPoint = result.maxLoc;
+        let color = new cv.Scalar(255, 0, 0, 255);
+        console.log("=====>" , result);
+        let matchedPoints = new cv.Point(maxPoint.x + marker.cols, maxPoint.y + marker.rows);
+/*
+        //  Step 3 : convert it to grayscale, and blur it slightly
         let imgSize = Math.max(src.cols, src.rows);
         let refined1 = new cv.Mat();
         let refined2 = new cv.Mat();
         cv.cvtColor(src, refined1, cv.COLOR_BGR2GRAY, 0);
         let blurSize = new cv.Size(7, 7);
         cv.GaussianBlur(refined1, refined2, blurSize, 0, 0, cv.BORDER_DEFAULT);
-        // Step 3: perform edge detection, then perform a dilation + erosion to
+        // Step 4: perform edge detection, then perform a dilation + erosion to
         // close gaps in between object edges
-        // Step 3.1 : edge detection
+        // Step 4.1 : edge detection
         cv.Canny(refined2, refined1, 50, 100, 3, false);
-        // Step 3.2 : dilation
+        // Step 4.2 : dilation
         const kernel = cv.getStructuringElement(cv.MORPH_RECT,new cv.Size(5,5));
         cv.dilate(refined1, refined2, kernel, new cv.Point(-1, 1), 1);
-        // Step 3.3 : erosion
+        // Step 4.3 : erosion
        /* TODO
-        var borderValue = cv.Scalar.all(Number.MAX_VALUE);
-        var erosion_type = cv.MorphShapes.MORPH_RECT.value;
-        var erosion_size = [2*Control.erosion_size+1, 2*Control.erosion_size+1];
-        var element = cv.getStructuringElement(erosion_type, erosion_size, [-1, -1]);
-        cv.erode(refined2, refined1, element, [-1, -1], 1, cv.BORDER_CONSTANT, borderValue);*/
-       
-        // Step 4: find coutours in the edge map
-        // Step 4.1 : find contours
+        var borderValue = cv.Scalar.all(Number.MAX_VALUE);v.imread(imgElement);
+        // Step 5.1 : find contours
         cv.threshold(refined2, refined1, 120, 200, cv.THRESH_BINARY);
-        let dst = resize(imgElement, fixedSize);
         let contours = new cv.MatVector();
         let hierarchy = new cv.Mat();
         let poly = new cv.MatVector();
         cv.findContours(refined1, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
 
-        // Step 4.2 : approximates each contour to polygon
+        // Step 5.2 : approximates each contour to polygon
         for (let i = 0; i < contours.size(); ++i) {
             let tmp = new cv.Mat();
             let cnt = contours.get(i);
@@ -64,7 +77,7 @@
             cnt.delete(); tmp.delete();
         }
 
-        // Step 5: bounding boxes (min area rects)
+        // Step 6: bounding boxes (min area rects)
         // Only keep "big" bounding boxes
         let results = [];
         for (let i = 0; i < poly.size(); ++i) {
@@ -85,11 +98,11 @@
                 results.push({'left_x': minX, 'center_x': rotatedRect.center.x, 'center_y': rotatedRect.center.y, 'size': rectangleSize});
             }
         }
-        // Step 6: sort from left to right
+        // Step 7: sort from left to right
         let sorted = results.sort((a,b) =>  a.left_x - b.left_x);
         console.log(sorted);
 
-        // Step 7: compute sizes
+        // Step 8: compute sizes
         let ratio = -1;
         let html1 =  "<table style='margin-left:50px;border:1px solid black'><tr><td style='border:1px solid black'>Size in pixels</td>";
         let html2 =  "<tr><td style='border:1px solid black'>Size in mm </td>";
@@ -106,13 +119,17 @@
         };
         html = html1 + '</tr>' + html2 + '</tr></table>';
         document.getElementById('status').innerHTML = html;
-        cv.imshow('canvasOutput1', src);origin
-        cv.imshow('canvasOutput2', refined1);
+
+        // Show matched tempalte*/
+        cv.rectangle(dst, maxPoint, matchedPoints, color, 2, cv.LINE_8, 0);
+
+        cv.imshow('canvasOutput1', src);
+        //cv.imshow('canvasOutput2', refined1);
         cv.imshow('canvasOutput3', dst);
 
         src.delete();
-        refined1.delete();
-        refined2.delete();
+        //refined1.delete();
+        //refined2.delete();
         dst.delete();
     }
 
