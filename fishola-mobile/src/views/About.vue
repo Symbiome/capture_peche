7363ac53815c0bdface9f28ed400121cd14504d1
Arnaud Thimel <thimel@codelutin.com>
2020-12-07 12:12:33 +0100

    Initialise et déclare la page d'accueil

---
+++ fishola-mobile/src/views/About.vue
@@ -0,0 +1,49 @@
+<!--
+  #%L
+  Fishola :: Mobile
+  %%
+  Copyright (C) 2019 - 2020 INRAE - UMR CARRTEL
+  %%
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  #L%
+  -->
+<template>
+  <div>
+    À boute !
+  </div>
+</template>
+
+<script lang="ts">
+
+import Constants from '@/services/Constants'
+import ProfileService from '@/services/ProfileService'
+import router from '@/router'
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export default class AboutView extends Vue {
+
+  mounted() {
+  }
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+
+@import "../less/main";
+
+</style>
