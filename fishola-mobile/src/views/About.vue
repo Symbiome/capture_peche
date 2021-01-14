b5716b8e2f188358741af577a5df0785f9b45b40
Arnaud Thimel <thimel@codelutin.com>
2021-01-14 15:29:17 +0100

    Mise à jour des liens Facebook et Twitter

--- fishola-mobile/src/views/About.vue
+++ fishola-mobile/src/views/About.vue
@@ -227,8 +227,8 @@
                                 <div class="clear"></div>
                                 <p>Retrouvez-nous sur :</p>
                                 <ul>
-                                    <li><a rel="nofollow" href="https://www.facebook.com/UMR-Carrtel-1625760484103771/" target="_blank"><img src="/img/facebook-icn.png" alt="Facebook"></a></li>
-                                    <li><a rel="nofollow" href="https://twitter.com/UmrCarrtel" target="_blank"><img src="/img/twitter-icn.png" alt="Twitter"></a></li>
+                                    <li><a rel="nofollow" href="https://www.facebook.com/Fishola-103641538377918" target="_blank"><img src="/img/facebook-icn.png" alt="Facebook"></a></li>
+                                    <li><a rel="nofollow" href="https://twitter.com/FisholaFr" target="_blank"><img src="/img/twitter-icn.png" alt="Twitter"></a></li>
                                     <li><a rel="nofollow" href="https://www.youtube.com/UmrCarrtel" target="_blank"><img src="/img/youtube-icn.png" alt="Youtube"></a></li>
                                 </ul>
                         </div>
