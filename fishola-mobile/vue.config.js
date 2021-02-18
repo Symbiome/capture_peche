d912bf65c1fbc8df20bf9c520ca5880cf57010c0
Votre Nom <thimel@codelutin.com>
2021-02-18 18:06:37 +0100

    Créé une variable pour s'adapter au mode

--- fishola-mobile/vue.config.js
+++ fishola-mobile/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // options...
-  outputDir: 'target/dist',
+  outputDir: 'target/dist-' + process.env.DIST_FOLDER,
   // Adding this allows to debug in firefox/chrome
   configureWebpack: {
     devtool: 'source-map'
