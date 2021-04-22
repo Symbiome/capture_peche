5e369771551c6bda47c96f60987575bcc54d5b43
Alex Morel <amorel@codelutin.com>
2021-04-22 16:47:54 +0200

    [Fish measure] Delete js folder

--- fishola-mobile/src/components/opencv/OpenCVSizeComputation.vue
+++ fishola-mobile/src/components/opencv/OpenCVSizeComputation.vue
@@ -20,44 +20,77 @@
   -->
 <template>
   <div class="page-with-header-and-footer shifted-background">
-    <FisholaHeader/>
+    <FisholaHeader />
     <div class="page">
       <div class="pane pane-only">
-
         <div class="pane-content large rounded">
-
-            <h1 class="no-margin-pane"> OpenCV - Mesure de poissons (Test) </h1>
-            <div class="measure-tests">
-                Lefmost object size(mm)
-                <input type="text"  style="width:100px"  id="leftSizeObjectSizeMm" value="133"/>
-                Resize image size
-                <input type="text"  style="width:100px"  id="fixedSize" value="150"/><br/>
-                Min % of detected forms (between 0 and 1)
-                <input type="text" style="width:100px" id="minCoverrage" value="0.15"/>
-                <br/>
-                <div class="inputoutput">
-                <img style="display:none" id="imageSrc" alt="No Image" />
-                <div class="caption">imageSrc <input type="file" id="fileInput" name="file" /></div>
-                </div>
-                <p id="status">OpenCV.js is loading...</p>
-                <div class="inputoutput">
-                    <div style="float:left;background-color:yellow;">
-                        <canvas  id="canvasOutput1" ></canvas><br/>
-                        <caption>Photo originale</caption>
-                    </div>
-                    <div style="clear:both"/>
-                        <div style="float:left;background-color:green;">
-                        <canvas  id="canvasOutput2" ></canvas><br/>
-                        <caption>Photo traitée</caption>
-                    </div>
-
-                    <div style="clear:both"/>
-                    <div style="float:left;background-color:green;">
-                        <canvas  id="canvasOutput3" ></canvas><br/>
-                        <caption>Calcul</caption>
-                    </div>
-                </div>
+          <h1 class="no-margin-pane">OpenCV - Mesure de poissons (Test)</h1>
+          <div class="measure-tests">
+            Lefmost object size(mm)
+            <input
+              type="text"
+              style="width:100px"
+              id="leftSizeObjectSizeMm"
+              :value="leftSizeObjectSizeMm"
+            />
+            Resize image size
+            <input
+              type="text"
+              style="width:100px"
+              id="fixedSize"
+              :value="fixedSize"
+            /><br />
+            Min % of detected forms (between 0 and 1)
+            <input
+              type="text"
+              style="width:100px"
+              id="minCoverrage"
+              :value="minCoverrage"
+            />
+            <br />
+            <div class="inputoutput">
+              <img
+                style="display:none"
+                id="imageSrc"
+                alt="No Image"
+                @load="onNewImageSourceLoad"
+                :src="imageSourceSRC"
+              />
+              <div class="caption">
+                imageSrc
+                <input
+                  type="file"
+                  id="fileInput"
+                  name="file"
+                  @change="changeSourceImage"
+                />
+              </div>
             </div>
+            <p id="status">OpenCV.js is loading...</p>
+            <div class="inputoutput">
+              <div style="float:left;background-color:yellow;">
+                <canvas id="canvasOutput1"></canvas><br />
+                <caption>
+                  Photo originale
+                </caption>
+              </div>
+              <div style="clear:both" />
+              <div style="float:left;background-color:green;">
+                <canvas id="canvasOutput2"></canvas><br />
+                <caption>
+                  Photo traitée
+                </caption>
+              </div>
+
+              <div style="clear:both" />
+              <div style="float:left;background-color:green;">
+                <canvas id="canvasOutput3"></canvas><br />
+                <caption>
+                  Calcul
+                </caption>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -65,27 +98,58 @@
 </template>
 
 <script lang="ts">
-
-import FisholaHeader from '@/components/layout/FisholaHeader.vue';
-import FisholaFooter from '@/components/layout/FisholaFooter.vue';
-import { Component, Vue } from 'vue-property-decorator';
+import FisholaHeader from "@/components/layout/FisholaHeader.vue";
+import FisholaFooter from "@/components/layout/FisholaFooter.vue";
+import { Component, Vue } from "vue-property-decorator";
+import FisholaOpenCVService from "@/services/opencv/fish-analyser.js";
+import cv from "@/services/opencv/opencv.js";
 
 @Component({
   components: {
     FisholaHeader,
-    FisholaFooter
-  }
+    FisholaFooter,
+  },
 })
 export default class OpenCVSizeComputation extends Vue {
+  imageSourceSRC = "";
+  minCoverrage = 0.15;
+  leftSizeObjectSizeMm = 133;
+  fixedSize = 150;
 
+  created(): void {
+    this.loadOpenCV();
+  }
+
+  async loadOpenCV() {
+    console.error("loadOpenCV");
+  }
+
+  changeSourceImage(e: Event): void {
+    console.error("changedSourceImage", e);
+    let eventTaget = e.target;
+    // @ts-ignore
+    if (eventTaget != null && eventTaget["files"]) {
+      // @ts-ignore
+      this.imageSourceSRC = URL.createObjectURL(eventTaget["files"][0]);
+    }
+  }
+
+  onNewImageSourceLoad(e: Event): void {
+    console.error("onNewImageSourceLoad ", e.target);
+    FisholaOpenCVService.INSTANCE.calculateSizes(cv, 
+      e.target,
+      this.minCoverrage,
+      this.leftSizeObjectSizeMm,
+      this.fixedSize
+    );
+  }
 }
 </script>
 
 <style scoped lang="less">
-
 @import "../../less/main";
 .measure-tests {
   padding: 20px;
-  padding-left:30px;
+  padding-left: 30px;
 }
 </style>
