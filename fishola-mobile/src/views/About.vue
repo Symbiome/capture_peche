c766c617db1b290ec8608c02493f407ff8dac2bb
Arnaud Thimel <thimel@codelutin.com>
2021-04-28 16:00:49 +0200

    Met à jour les chiffres en cas d'indispo du backend

--- fishola-mobile/src/views/About.vue
+++ fishola-mobile/src/views/About.vue
@@ -341,11 +341,11 @@ export default class AboutView extends Vue {
   titleText:string = "est l'application smartphone pour une gestion durable de la pêche sur les lacs alpins (Léman, lac d’Annecy, du Bourget et d’Aiguebelette).";
   contributeText:string = "<p>Le plus simple est de télécharger l'application et de l'utiliser pour saisir vos captures.</p>";
   tripsCount:number = 0;
-  realTripsCount:number = 302;
+  realTripsCount:number = 442;
   catchsCount:number = 0;
-  realCatchsCount:number = 1242;
+  realCatchsCount:number = 1726;
   picturesCount:number = 0;
-  realPicturesCount:number = 172;
+  realPicturesCount:number = 212;
   lakes:Lake[] = [];
   catchsCountPerLakeId: { [index: string]: number } = {};
 
