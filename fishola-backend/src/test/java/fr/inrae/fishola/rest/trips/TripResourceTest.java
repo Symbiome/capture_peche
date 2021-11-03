5a9344bdc23a32d982ced837c5899b744936b8ae
Arnaud Thimel <thimel@codelutin.com>
2021-11-03 19:45:21 +0100

    Ajout de test autour de l'ajout et la lecture d'image

--- fishola-backend/src/test/java/fr/inrae/fishola/rest/trips/TripResourceTest.java
+++ fishola-backend/src/test/java/fr/inrae/fishola/rest/trips/TripResourceTest.java
@@ -21,6 +21,7 @@ package fr.inrae.fishola.rest.trips;
  * #L%
  */
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableSet;
 import fr.inrae.fishola.database.ReferentialDao;
 import fr.inrae.fishola.database.TripsDao;
@@ -33,14 +34,24 @@ import fr.inrae.fishola.rest.AbstractFisholaResource;
 import fr.inrae.fishola.rest.AbstractFisholaTest;
 import fr.inrae.fishola.rest.JwtHelper;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.ResponseBodyExtractionOptions;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.core.MediaType;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -88,7 +99,7 @@ public class TripResourceTest extends AbstractFisholaTest {
     @Test
     public void testListMyTrips() {
         int count = countTrips();
-        Assertions.assertEquals(0, count);
+
         given()
             .when()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -173,6 +184,126 @@ public class TripResourceTest extends AbstractFisholaTest {
         // La création n'a pas pu se faire, on vérifie que la sortie n'a pas été créée
         int countAfter = countTrips();
         Assertions.assertEquals(countBefore, countAfter);
+
+    }
+
+    @Test
+    public void testTripWithPicture() throws IOException {
+
+        int countBefore = countTrips();
+
+        TripBean trip = buildValidTripBean();
+        CatchBean c = new CatchBean();
+        c.id = "abc";
+        c.speciesId = Optional.of(trip.speciesIds.iterator().next().toString());
+        c.techniqueId = trip.techniqueIds.iterator().next();
+        trip.catchs = Collections.singletonList(c);
+
+        ResponseBodyExtractionOptions body = given()
+            .when()
+                .contentType(MediaType.APPLICATION_JSON)
+                .cookie(AbstractFisholaResource.USER_AUTHENTICATION_COOKIE_NAME, token)
+                .body(trip)
+                .post("/api/v1/trips")
+            .then()
+                .statusCode(201)
+                // On reçoit la map de replacement :
+                //   dontcare -> nouvel-id-de-trip
+                //   abc -> nouvel-id-de-capture
+                // telle que :
+                //  {"dontcare": "2244331f-f9dc-4102-b832-be7d69b7c377", "abc": "c2ec85c3-7b03-4cc7-ad80-5e17cfa29772"}
+                .body(trip.id, notNullValue())
+                .body(c.id, notNullValue())
+            .extract()
+                .body();
+        String tripId = body.path(trip.id);
+        String catchId = body.path(c.id);
+
+        int countAfter = countTrips();
+        Assertions.assertEquals(countBefore + 1, countAfter);
+
+        // Pas encore d'image -> 404
+        given()
+            .when()
+                .body(trip)
+                .get("/api/v1/pictures/" + catchId)
+            .then()
+                .statusCode(404);
+
+        // {id='70d6bed0-72e4-49c4-8e1d-8c1278c94e17', createdOn=Optional[2021-11-03T19:08:34.338806], mode=Live, type=Craft, name='Whatever', lakeId=658c488f-b982-4f4c-8610-527a1684b3be, speciesIds=[d0176668-c2b5-4182-863c-f950b908d96a], otherSpecies='null', date=2021-11-03, startedAt=00:00, finishedAt=00:01, weatherId=Optional.empty, catchs=1, techniqueIds=[9929f857-ecc1-40a9-859f-91b897cdb12c], beginLatitude=Optional.empty, beginLongitude=Optional.empty, endLatitude=Optional.empty, endLongitude=Optional.empty, source=null, saveDelayMarker=Optional.empty, modifiableUntil=Optional[2021-11-10T19:08:34.338806]}
+        given()
+            .when()
+                .cookie(AbstractFisholaResource.USER_AUTHENTICATION_COOKIE_NAME, token)
+                .get("/api/v1/trips/" + tripId)
+            .then()
+                .statusCode(200)
+                .body("id", equalTo(tripId))
+                .body("date[0]", equalTo(trip.date.getYear()))
+                .body("date[1]", equalTo(trip.date.getMonthValue()))
+                .body("date[2]", equalTo(trip.date.getDayOfMonth()))
+                .body("startedAt", equalTo("00:00"))
+                .body("finishedAt", equalTo("00:01"))
+                .body("mode", equalTo(trip.mode.getLiteral()))
+                .body("type", equalTo(trip.type.getLiteral()))
+                .body("catchs[0].hasPicture", equalTo(false));
+
+        InputStream resource = this.getClass().getResourceAsStream("/about-fishes.jpg");
+        Preconditions.checkState(resource != null, "Image non trouvée");
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        IOUtils.copy(resource, stream);
+        byte[] originalByteArray = stream.toByteArray();
+        String base64Content = Base64.getEncoder().encodeToString(originalByteArray);
+
+        given()
+            .when()
+                .contentType(MediaType.TEXT_PLAIN)
+                .cookie(AbstractFisholaResource.USER_AUTHENTICATION_COOKIE_NAME, token)
+                .body("data:image/jpeg;base64," + base64Content)
+                .put("/api/v1/pictures/" + catchId)
+            .then()
+                .statusCode(204);
+
+        // On vérifie que la 404 a disparu
+        byte[] bytes =
+            given()
+                .when()
+                    .body(trip)
+                    .get("/api/v1/pictures/" + catchId)
+                .then()
+                    .statusCode(200)
+                .extract()
+                    .body()
+                    .asByteArray();
+
+        // On compare les dimensions de l'image lue avec l'image d'origine
+        ByteArrayInputStream originalInputStream = new ByteArrayInputStream(originalByteArray);
+        BufferedImage originalImage = ImageIO.read(originalInputStream);
+
+        ByteArrayInputStream newInputStream = new ByteArrayInputStream(bytes);
+        BufferedImage newImage = ImageIO.read(newInputStream);
+
+        Assertions.assertEquals(1920, originalImage.getWidth());
+        Assertions.assertEquals(originalImage.getWidth(), newImage.getWidth());
+        Assertions.assertEquals(680, originalImage.getHeight());
+        Assertions.assertEquals(originalImage.getHeight(), newImage.getHeight());
+
+        // On vérifie que la miniature est générée
+        given()
+            .when()
+                .body(trip)
+                .get("/api/v1/pictures/" + catchId + "/preview")
+            .then()
+                .statusCode(200);
+
+        // {id='70d6bed0-72e4-49c4-8e1d-8c1278c94e17', createdOn=Optional[2021-11-03T19:08:34.338806], mode=Live, type=Craft, name='Whatever', lakeId=658c488f-b982-4f4c-8610-527a1684b3be, speciesIds=[d0176668-c2b5-4182-863c-f950b908d96a], otherSpecies='null', date=2021-11-03, startedAt=00:00, finishedAt=00:01, weatherId=Optional.empty, catchs=1, techniqueIds=[9929f857-ecc1-40a9-859f-91b897cdb12c], beginLatitude=Optional.empty, beginLongitude=Optional.empty, endLatitude=Optional.empty, endLongitude=Optional.empty, source=null, saveDelayMarker=Optional.empty, modifiableUntil=Optional[2021-11-10T19:08:34.338806]}
+        given()
+            .when()
+                .cookie(AbstractFisholaResource.USER_AUTHENTICATION_COOKIE_NAME, token)
+                .get("/api/v1/trips/" + tripId)
+            .then()
+                .statusCode(200)
+                .body("id", equalTo(tripId))
+                .body("catchs[0].hasPicture", equalTo(true));
 
     }
 
