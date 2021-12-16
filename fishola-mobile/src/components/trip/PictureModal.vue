7a1c71c40dd0aac6691035149afacd1cdd886c85
Alex Morel <amorel@codelutin.com>
2021-12-16 10:02:33 +0100

    [Gallery-Front] Mobile gallery - Step 2: display slides

--- fishola-mobile/src/components/trip/PictureModal.vue
+++ fishola-mobile/src/components/trip/PictureModal.vue
@@ -26,9 +26,11 @@
         <div class="pane-content">
           <h2 class="title">Galerie Photos</h2>
           <!-- pictures required for measurement -->
-          <div class="picture-holder">
-            <img class="picture-display" :src="focusedPicSrc" />
-          </div>
+          <PictureModalMobileGallerySlider
+            :src="focusedPicSrc"
+            :otherPics="otherPics"
+            :measurementPictureSrc="measurementPictureSrc"
+          />
         </div>
       </div>
     </div>
@@ -107,10 +109,12 @@
 import PictureContentWithOrder from "@/pojos/PictureContentWithOrder";
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 import PicturePreview from "@/components/trip/PicturePreview.vue";
+import PictureModalMobileGallerySlider from "@/components/trip/PictureModalMobileGallerySlider.vue";
 
 @Component({
   components: {
     PicturePreview,
+    PictureModalMobileGallerySlider,
   },
 })
 export default class PictureModal extends Vue {
@@ -309,96 +313,6 @@ export default class PictureModal extends Vue {
     border-top-left-radius: 30px;
     border-top-right-radius: 30px;
     padding-bottom: 10vh;
-
-    .bottom-actions {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      background-color: @solitude;
-      padding-left: @margin-large;
-      padding-right: @margin-large;
-      padding-bottom: @margin-medium;
-    }
-
-    .validate-redo {
-      padding-bottom: 20px;
-      padding-top: 30px;
-    }
-
-    .picture-holder {
-      background-color: @gainsboro;
-    }
-    .picture-display {
-      max-height: 40vh;
-      max-width: 90vw;
-      padding-left: 5vw;
-      display: block;
-      margin-left: auto;
-      margin-right: auto;
-    }
-
-    .error {
-      color: @cardinal;
-    }
-    .success {
-      color: @lime-green;
-    }
-
-    .measure {
-      float: left;
-      padding-right: 10px;
-    }
-  }
-
-  @keyframes spin {
-    100% {
-      -webkit-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
-
-  .spinner {
-    height: 60px;
-    width: 60px;
-    border-radius: 50%;
-    border-top: 3px solid @pelorous;
-    border-left: 3px solid @pelorous;
-    animation: spin 2s linear infinite;
-    position: absolute;
-    top: 48vh;
-    left: calc(50vw - 30px);
-    display: block;
-  }
-
-  .button-main {
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: -55px;
-  }
-
-  .button-minor-left {
-    margin-right: auto;
-    float: left;
-    button {
-      color: @pelorous;
-      font-weight: bold;
-      background-color: rgba(0, 0, 0, 0);
-      border: 0px solid black;
-      font-size: 18px;
-    }
-  }
-
-  .button-minor-right {
-    margin-left: auto;
-    float: right;
-    button {
-      color: @pelorous;
-      font-weight: bold;
-      background-color: rgba(0, 0, 0, 0);
-      border: 0px solid black;
-      font-size: 16px;
-    }
   }
 }
 </style>
