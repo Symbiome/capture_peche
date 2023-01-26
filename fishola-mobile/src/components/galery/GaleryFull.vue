33a20794d2c7ba4743a72763b9d6bdc7094f6536
Alex Morel <amorel@codelutin.com>
2023-01-26 09:18:23 +0100

    Keep galery lake selection

--- fishola-mobile/src/components/galery/GaleryFull.vue
+++ fishola-mobile/src/components/galery/GaleryFull.vue
@@ -308,7 +308,11 @@ export default class GaleryFull extends Vue {
   async seeTrip(tripId: string) {
     router.push({
       name: "trip-summary",
-      params: { id: tripId, fromGallery: "true" },
+      params: {
+        id: tripId,
+        fromGallery: "true",
+        lakeFilter: this.selectedLakeUUID,
+      },
     });
   }
 
