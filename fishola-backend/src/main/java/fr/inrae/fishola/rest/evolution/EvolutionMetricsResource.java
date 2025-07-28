351bfa0c89d28c14e3bf70a5e5d3e5b5134dcdc9
Alex Morel <amorel@codelutin.com>
2025-07-28 14:30:00 +0200

    [Build] Add copyrights where missing

--- fishola-backend/src/main/java/fr/inrae/fishola/rest/evolution/EvolutionMetricsResource.java
+++ fishola-backend/src/main/java/fr/inrae/fishola/rest/evolution/EvolutionMetricsResource.java
@@ -1,4 +1,25 @@
 package fr.inrae.fishola.rest.evolution;
+
+/*-
+ * #%L
+ * Fishola :: Backend
+ * %%
+ * Copyright (C) 2019 - 2025 INRAE - UMR CARRTEL
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 import fr.inrae.fishola.database.EvolutionDao;
 import fr.inrae.fishola.rest.AbstractFisholaResource;
 import fr.inrae.fishola.rest.FisholaCache;
