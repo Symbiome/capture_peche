d52d554e02c9b5198c4ce541b996f0e7c842c70e
Léo Kaufmann <kaufmann@codelutin.com>
2025-08-05 12:34:12 +0200

    ref #11673 - Improve graph display in dashboard evolution screen

--- fishola-mobile/src/components/charts/evolution/EvolutionMetrics.vue
+++ fishola-mobile/src/components/charts/evolution/EvolutionMetrics.vue
@@ -19,25 +19,23 @@
   #L%
   -->
 <template>
-    <div class="section shrinked">
-        <div v-if="!containsData">
-          Aucune donnée pour ce lac.
-        </div>
-        <div v-else>
-            <select v-model="displayMode" @change="switchMode">
-              <option value="tripsCount">Nombre de sorties avec au moins une prise</option>
-              <option value="totalCatchesCount">Nombre d'individus capturés (Total)</option>
-              <option value="keptCatchesCount">Nombre d'individus capturés (et conservés)</option>
-            </select>
-            <Bar v-if="chartData && chartOptions" :data="chartData" :options="chartOptions" ref='chart' />
-            <h3>TODO ajouter lien vers la dashboard pole ECLA </h3>
-        </div>
+    <div v-if="!containsData" class="not-enough-data">
+      Aucune donnée pour ce lac.
+    </div>
+    <div v-else  id="evolution-graph">
+        <select v-model="displayMode" @change="switchMode">
+          <option value="tripsCount">Nombre de sorties avec au moins une prise</option>
+          <option value="totalCatchesCount">Nombre d'individus capturés (Total)</option>
+          <option value="keptCatchesCount">Nombre d'individus capturés (et conservés)</option>
+        </select>
+        <Bar v-if="chartData && chartOptions" :data="chartData" :options="chartOptions" ref='chart' />
+        <h3>TODO ajouter lien vers la dashboard pole ECLA </h3>
     </div>
 </template>
 
 <script lang="ts">
 
-import { EvolutionMetricsForLake, Month, SpeciesWithAlias } from '@/pojos/BackendPojos';
+import { EvolutionMetricsForLake, EvolutionMetricForSpecieAndMonth, SpeciesWithAlias } from '@/pojos/BackendPojos';
 import DashboardService from '@/services/DashboardService';
 import ReferentialService from '@/services/ReferentialService';
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
@@ -52,7 +50,6 @@ import {
   LinearScale
 } from 'chart.js'
 import { Bar } from 'vue-chartjs'
-import { months } from 'moment';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
@@ -70,8 +67,9 @@ export default class EvolutionMetricsView extends Vue {
   displayMode = 'tripsCount';
   containsData:boolean = false;
   chartData : any | null = null;
-  chartOptions : any | null =  {
+  chartOptions : any | null = {
     responsive: true,
+    maintainAspectRatio: false,
     parsing: {
       xAxisKey: 'monthYear',
       yAxisKey: this.displayMode
@@ -79,11 +77,28 @@ export default class EvolutionMetricsView extends Vue {
     scales: {
       x: {
         stacked: true,
+        grid: {
+            color: 'transparent'
+        },
+        border: {
+            color: '#1E9BC4'
+        }
       },
       y: {
-        stacked: true
+        stacked: true,
+        grid: {
+            color: '#DFE6E9'
+        },
+        border: {
+            color: '#1E9BC4'
+        }
       }
     },
+    plugins: {
+      legend: {
+        position: 'bottom'
+      },
+    }
   };
 
   evolutionMetrics: EvolutionMetricsForLake = {
@@ -119,16 +134,60 @@ export default class EvolutionMetricsView extends Vue {
     }
   }
 
+  extractYearFromMetric(metric: EvolutionMetricForSpecieAndMonth) {
+    const regexp = /20\d{2}$/g;
+    let parsedYear = metric.monthYear.match(regexp);
+    if (parsedYear) {
+      return parseInt(parsedYear[0]);
+    }
+    return new Date().getFullYear();
+  }
+
+  /* Determines the years to display based on the evolution data fetched */
   getYears() : string[] {
-    return ['2020', '2021', '2022', '2023', '2024'];
+    const currentYear = new Date().getFullYear();
+    let earliestYear = currentYear;
+
+    for (const specie in this.evolutionMetrics.evolutionPerMonthAndSpecie) {
+      if (this.evolutionMetrics.evolutionPerMonthAndSpecie[specie].length) {
+        let metricWithEarliestYear = this.evolutionMetrics.evolutionPerMonthAndSpecie[specie].reduce((min, obj) => {
+          const year = this.extractYearFromMetric(obj);
+          const yearMin = this.extractYearFromMetric(min)
+          return  year < yearMin ? obj : min;
+        })
+        let earliestYearForSpecie = this.extractYearFromMetric(metricWithEarliestYear);
+        if (earliestYear > earliestYearForSpecie) {
+          earliestYear = earliestYearForSpecie;
+        }
+      }
+    }
+    let years : string[] = [];
+    while(earliestYear <= currentYear) {
+      years.push((earliestYear++).toString());
+    }
+    return years;
   }
 
   getMonths() : string[] {
-      // if (year && this.evolutionMetrics.catchCountPerMonthAndSpecies[year]) {
-      // return Object.keys(this.evolutionMetrics.catchCountPerMonthAndSpecies[year]) as Month[];
-      // }
-      // return [];
+    // Harcoded to match generated string from backend : java.time.Month getDisplayName(TextStyle.SHORT, Locale.FRANCE)
     return ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'];
+  }
+
+  getLabels() {
+    let labels : any[] = [];
+    let years = this.getYears();
+    let monthsFr = this.getMonths()
+    let currentMonthIndex = new Date().getMonth();
+    let currentYear = new Date().getFullYear();
+
+    years.forEach((y, i) => {
+      monthsFr.forEach((m, j) => {
+        if (i < currentYear || j < currentMonthIndex) {
+          labels.push(m + ' ' + y)
+        }
+      });
+    })
+    return labels;
   }
 
   getSpecieNameForLake(specieId: string) {
@@ -138,17 +197,8 @@ export default class EvolutionMetricsView extends Vue {
     return specie ? (specie.alias ?? specie.name) : specieId;
   }
 
-  initChartData() {
-
-    let years = this.getYears();
-    let monthsFr = this.getMonths();
-    let labels : any[] = [];
-    years.forEach(y => {
-      monthsFr.forEach(m => {
-        labels.push(m + ' ' + y)
-      });
-    })
-
+  initChartData() {;
+    let labels : any[] = this.getLabels();
     let datasets : any[] = [];
     let colors = ['#1971c2', '#099268', '#66a80f', '#a9e34b', '#ffd43b', '#9c36b5', '#e03131',
         '#4dabf7', '#38d9a9', '#a9e34b', '#ffd43b', '#ffd43b', '#da77f2', '#ff8787'];
@@ -172,29 +222,35 @@ export default class EvolutionMetricsView extends Vue {
   }
 
   switchMode() {
-    this.chartOptions = {
-      responsive: true,
-      parsing: {
-        xAxisKey: 'monthYear',
-        yAxisKey: this.displayMode
-      },
-      scales: {
-        x: {
-          stacked: true,
-        },
-        y: {
-          stacked: true
-        }
-      },
-    }
+    this.chartOptions = {...this.chartOptions};
     /* The following instructions is added to be sure the chart is updated with the correct option change */
-    this.$refs.chart.options.parsing.yAxisKey = this.displayMode
+    this.$refs.chart.options.parsing.xAxisKey = this.displayMode
   }
 }
 </script>
 
 <style scoped lang="less">
 @import "../../../less/main";
+
+.not-enough-data {
+  font-style: italic;
+  font-size: @fontsize-button;
+  line-height: calc(@fontsize-button + @line-height-padding-x-large);
+  color: @pale-sky;
+  text-align: center;
+  margin-top: @vertical-margin-large;
+}
+
+#evolution-graph {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  margin: 0;
+  min-height: 500px;
+  min-height: 40vh;
+  max-height: 80%;
+}
 
 select {
   background: transparent;
@@ -204,10 +260,15 @@ select {
   border-radius: 20px;
   margin-left: 10px;
   font-weight: bold;
-  font-size: 16px;
+  font-size: 14px;
   font-family: inherit;
   color: @pelorous;
   cursor: pointer;
+  max-width: 80vw;
+
+  @media screen and (min-width: @desktop-min-width) {
+    font-size: 16px;
+  }
 
   option {
     color: black;
