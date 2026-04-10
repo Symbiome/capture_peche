c7e677f5944842b2ebf86b94cc69d31891429017
lcazef <caze@codelutin.com>
2026-04-10 10:41:07 +0200

    feat(composition): ImageUploader.vue

--- fishola-admin/src/components/ImageUploader.vue
+++ fishola-admin/src/components/ImageUploader.vue
@@ -31,45 +31,48 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-facing-decorator";
+<script setup lang="ts">
 import BackendService from "@/services/BackendService";
 import Constants from "@/services/Constants";
+import {useTemplateRef} from "vue";
 
-@Component
-export default class ImageUploader extends Vue {
-  @Prop() itemId: string;
-  @Prop() isMiniature: boolean;
+const props = defineProps<{
+  itemId: string,
+  isMiniature: boolean,
+}>();
 
-  async uploadImageFile(e: InputEvent): Promise<void> {
-    const uploadedFile = this.$refs.upload as HTMLInputElement;
-    if (uploadedFile.files) {
-      const file = uploadedFile.files[0];
-      const base64 = await this.getBase64(file);
-      const id = this.itemId ? this.itemId : "temp-id";
-      let url = "/v1/news-picture/";
-      if (this.isMiniature) {
-        url = "/v1/news-miniature/";
-      }
-      const newsPicture = await BackendService.backendPost(url + id, base64);
-      const newsPicturesURL = Constants.apiUrl(
-        "/v1/news-picture/" + newsPicture["id"]
-      );
-      this.$emit("uploaded-pic", newsPicturesURL);
+const emit = defineEmits<{
+  (e: "uploaded-pic", url: string): void,
+}>();
+
+async function uploadImageFile(_: InputEvent): Promise<void> {
+  const uploadedFile = useTemplateRef("upload").value as HTMLInputElement;
+  if (uploadedFile.files) {
+    const file = uploadedFile.files[0];
+    const base64 = await getBase64(file);
+    const id = props.itemId ? props.itemId : "temp-id";
+    let url = "/v1/news-picture/";
+    if (props.isMiniature) {
+      url = "/v1/news-miniature/";
     }
+    const newsPicture = await BackendService.backendPost(url + id, base64);
+    const newsPicturesURL = Constants.apiUrl(
+      "/v1/news-picture/" + newsPicture["id"]
+    );
+    emit("uploaded-pic", newsPicturesURL);
   }
+}
 
-  getBase64(file: any): Promise<string> {
-    return new Promise<any>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = function() {
-        resolve(reader.result);
-      };
-      reader.onerror = function(error) {
-        reject(error);
-      };
-    });
-  }
+function getBase64(file: any): Promise<string> {
+  return new Promise<any>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function() {
+      resolve(reader.result);
+    };
+    reader.onerror = function(error) {
+      reject(error);
+    };
+  });
 }
 </script>
