bca5c131f4aa45b8dfa0491e2194f1ef9bc6049a
lcazef <caze@codelutin.com>
2026-04-13 11:21:01 +0200

    feat(composition): SpeciesPerLake.vue

--- fishola-admin/src/views/customize/SpeciesPerLake.vue
+++ fishola-admin/src/views/customize/SpeciesPerLake.vue
@@ -30,8 +30,9 @@
       />
     </div>
 
-    <b-message type="is-info" >
-      Cochez les espèces présentes sur le plan d'eau. Vous pouvez préciser un nom spécifique si différent du nom d'espèce (e.g. le "corégone" est appelé "Lavaret" sur le lac d'Aiguebelette et "Féran" sur le Léman)
+    <b-message type="is-info">
+      Cochez les espèces présentes sur le plan d'eau. Vous pouvez préciser un nom spécifique si différent du nom
+      d'espèce (e.g. le "corégone" est appelé "Lavaret" sur le lac d'Aiguebelette et "Féran" sur le Léman)
     </b-message>
     <table
       class="table is-striped"
@@ -41,20 +42,34 @@
       <thead>
         <tr>
           <th id="empty-lake-cell"></th>
-          <th :id="l.name" v-for="l in selectedLakes" v-bind:key="l.id">
+          <th
+            :id="l.name"
+            v-for="l in selectedLakes"
+            v-bind:key="l.id"
+          >
             {{ l.name }}
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="s in species" v-bind:key="s.id">
+        <tr
+          v-for="s in species"
+          v-bind:key="s.id"
+        >
           <th :id="s.name">{{ s.name }}</th>
-          <td v-for="l in selectedLakes" v-bind:key="l.id">
-            <div class="field" style="display:flex;gap:5px">
-              <input type="checkbox"
+          <td
+            v-for="l in selectedLakes"
+            v-bind:key="l.id"
+          >
+            <div
+              class="field"
+              style="display:flex;gap:5px"
+            >
+              <input
+                type="checkbox"
                 :checked="!speciesPerLakeAbsent[l.id] || speciesPerLakeAbsent[l.id].indexOf(s.id) === -1"
                 @click="toggleAbsent(l.id, s.id)"
-                />
+              />
               <div v-if="speciesPerLakeAbsent[l.id] && speciesPerLakeAbsent[l.id].indexOf(s.id) > -1">
                 Espèce non présente
               </div>
@@ -69,120 +84,122 @@
       </tbody>
     </table>
     <div class="buttons">
-      <button class="button is-primary" @click="save()">Enregistrer</button>
+      <button
+        class="button is-primary"
+        @click="save()"
+      >Enregistrer</button>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-facing-decorator";
+<script setup lang="ts">
+import { getCurrentInstance, ref, Ref } from "vue";
 
 import BackendService from "@/services/BackendService";
 import MultipleAutoComplete from "@/components/MultipleAutoComplete.vue";
+import { useToast } from "buefy";
 
-@Component({ components: { MultipleAutoComplete } })
-export default class SpeciesPerLakeVue extends Vue {
-  lakes = [];
-  species = [];
-  speciesPerLake = {};
-  speciesPerLakeAliases = {};
-  speciesPerLakeAbsent = {};
-  lakeSelectionOptions = [];
-  lastLakeSelection = [];
-  selectedLakes = [];
-  maxLakeBeforeShowingAutoComplete = 5;
+const Toast = useToast();
 
-  created() {
-    this.lastLakeSelection = JSON.parse(
-      localStorage.getItem("lastLakeSelection") ?? "[]"
-    );
-    Promise.all([
-      BackendService.backendGet("/v1/referential/lakes"),
-      BackendService.backendGet("/v1/referential/species"),
-      BackendService.backendGet("/v1/referential/species-per-lake"),
-    ]).then((data) => {
-      this.lakes = data[0];
-      this.species = data[1];
-      this.speciesPerLake = data[2];
-      this.lakeSelectionOptions = [];
-      this.lakes.forEach((l) => {
-        this.lakeSelectionOptions.push({ id: l.id, label: l.name });
-      });
-      if (this.lakes.length < this.maxLakeBeforeShowingAutoComplete) {
-        this.selectedLakes = this.lakes;
-      }
-      this.referentialLoaded();
-    });
+const lakes = ref([]);
+const species = ref([]);
+const speciesPerLake: Ref<any> = ref({});
+const speciesPerLakeAliases: Ref<any> = ref({});
+const speciesPerLakeAbsent: Ref<any> = ref({});
+const lakeSelectionOptions: Ref<any[]> = ref(JSON.parse(
+  localStorage.getItem("lastLakeSelection") ?? "[]"
+));
+const lastLakeSelection = ref([]);
+const selectedLakes = ref([]);
+const maxLakeBeforeShowingAutoComplete = 5;
+
+
+Promise.all([
+  BackendService.backendGet("/v1/referential/lakes"),
+  BackendService.backendGet("/v1/referential/species"),
+  BackendService.backendGet("/v1/referential/species-per-lake"),
+]).then((data) => {
+  lakes.value = data[0];
+  species.value = data[1];
+  speciesPerLake.value = data[2];
+  lakeSelectionOptions.value = [];
+  lakes.value.forEach((l) => {
+    lakeSelectionOptions.value.push({ id: l.id, label: l.name });
+  });
+  if (lakes.value.length < maxLakeBeforeShowingAutoComplete.value) {
+    selectedLakes.value = lakes.value;
   }
+  referentialLoaded();
+});
 
-  referentialLoaded() {
-    this.lakes.forEach((l) => {
-      this.speciesPerLakeAliases[l.id] = {};
-    });
+function referentialLoaded() {
+  lakes.value.forEach((l) => {
+    speciesPerLakeAliases.value[l.id] = {};
+  });
 
-    Object.keys(this.speciesPerLake).forEach((lakeId) => {
-      let items = this.speciesPerLake[lakeId];
-      items.forEach((spl) => {
-        if (!spl.present) {
-          if (!this.speciesPerLakeAbsent[lakeId]) {
-            this.speciesPerLakeAbsent[lakeId] = [spl.id];
-          } else {
-            this.speciesPerLakeAbsent[lakeId].push(spl.id);
-          }
-        } else if (spl.alias) {
-          this.speciesPerLakeAliases[lakeId][spl.id] = spl.alias;
+  Object.keys(speciesPerLake.value).forEach((lakeId) => {
+    const items = speciesPerLake.value[lakeId];
+    items.forEach((spl) => {
+      if (!spl.present) {
+        if (!speciesPerLakeAbsent.value[lakeId]) {
+          speciesPerLakeAbsent.value[lakeId] = [spl.id];
+        } else {
+          speciesPerLakeAbsent.value[lakeId].push(spl.id);
         }
-      });
+      } else if (spl.alias) {
+        speciesPerLakeAliases.value[lakeId][spl.id] = spl.alias;
+      }
     });
-  }
+  });
+}
 
-  save() {
-    BackendService.backendPut("/v1/referential/species-aliases-per-lake", {
-      targetLakes: this.selectedLakes.map((l) => l.id),
-      speciesPerLakeAliases: this.speciesPerLakeAliases,
-      speciesPerLakeAbsent: this.speciesPerLakeAbsent,
-    }).then(
-      (res) => {
-        this.$buefy.toast.open({
-          message: "Espèces par plan d'eau enregistrées",
-          type: "is-success",
-        });
-      },
-      (error) => {
-        this.$buefy.toast.open({
-          message:
-            "Erreur lors de l'enregistrement des espèces par plan d'eau : " +
-            error.message,
-          type: "is-danger",
-        });
-      }
-    );
-  }
-
-  changeLakeSelection(newSelectedLakeIds: string[]) {
-    this.selectedLakes = this.lakes.filter(
-      (l) => newSelectedLakeIds.indexOf(l.id) > -1
-    );
-    localStorage.setItem(
-      "lastLakeSelection",
-      JSON.stringify(newSelectedLakeIds)
-    );
-  }
-
-  toggleAbsent(lakeId: string, specieId: string) {
-    if (this.speciesPerLakeAbsent[lakeId]) {
-      const index = this.speciesPerLakeAbsent [lakeId].indexOf(specieId);
-      if (index === -1) {
-        this.speciesPerLakeAbsent[lakeId].push(specieId);
-      } else {
-        this.speciesPerLakeAbsent[lakeId].splice(index, 1);
-        this.speciesPerLakeAliases[lakeId][specieId] = undefined;
-      }
-    } else {
-      this.speciesPerLakeAbsent[lakeId] = [specieId];
+function save() {
+  BackendService.backendPut("/v1/referential/species-aliases-per-lake", {
+    targetLakes: selectedLakes.value.map((l) => l.id),
+    speciesPerLakeAliases: speciesPerLakeAliases.value,
+    speciesPerLakeAbsent: speciesPerLakeAbsent.value,
+  }).then(
+    (res) => {
+      Toast.open({
+        message: "Espèces par plan d'eau enregistrées",
+        type: "is-success",
+      });
+    },
+    (error) => {
+      Toast.open({
+        message:
+          "Erreur lors de l'enregistrement des espèces par plan d'eau : " +
+          error.message,
+        type: "is-danger",
+      });
     }
-    this.$forceUpdate();
+  );
+}
+
+function changeLakeSelection(newSelectedLakeIds: string[]) {
+  selectedLakes.value = lakes.value.filter(
+    (l) => newSelectedLakeIds.indexOf(l.id) > -1
+  );
+  localStorage.setItem(
+    "lastLakeSelection",
+    JSON.stringify(newSelectedLakeIds)
+  );
+}
+
+function toggleAbsent(lakeId: string, specieId: string) {
+  if (speciesPerLakeAbsent.value[lakeId]) {
+    const index = speciesPerLakeAbsent.value[lakeId].indexOf(specieId);
+    if (index === -1) {
+      speciesPerLakeAbsent.value[lakeId].push(specieId);
+    } else {
+      speciesPerLakeAbsent.value[lakeId].splice(index, 1);
+      speciesPerLakeAliases.value[lakeId][specieId] = undefined;
+    }
+  } else {
+    speciesPerLakeAbsent.value[lakeId] = [specieId];
   }
+  const instance = getCurrentInstance();
+  instance?.proxy?.$forceUpdate();
 }
 </script>
 
