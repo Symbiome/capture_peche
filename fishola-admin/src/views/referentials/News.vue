3602cf56cdc60c370ab513f8b8eccf5dd859aef2
lcazef <caze@codelutin.com>
2026-04-13 11:50:48 +0200

    feat(composition): News.vue

--- fishola-admin/src/views/referentials/News.vue
+++ fishola-admin/src/views/referentials/News.vue
@@ -35,142 +35,137 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Referential from "@/components/Referential.vue";
-import { Component, Vue } from "vue-facing-decorator";
 
 import { LocalDateTime, ZoneOffset, nativeJs } from "@js-joda/core";
 import BackendService from "@/services/BackendService";
 import Constants from "@/services/Constants";
 
 import router from "@/router";
+import { useDialog, useToast } from "buefy";
+import { reactive, Reactive, ref, Ref } from "vue";
 
-@Component({
-  components: {
-    Referential
-  }
-})
-export default class DocumentationVue extends Vue {
-  lakesIdToNameMap = new Map<string, string>();
-  lakesOptions = [];
-  docColumns = [];
-  loggedAdmin = { }
+const Dialog = useDialog();
+const Toast = useToast();
 
-  nextPlannifiedDate: number[] = [];
+const lakesIdToNameMap = reactive(new Map<string, string>());
+const lakesOptions: Reactive<any[]> = reactive([]);
+const docColumns: Ref<any[]> = ref([]);
+const loggedAdmin: Ref<any> = ref({});
+const nextPlannifiedDate: Ref<number[]> = ref([]);
 
-  created() {
-    this.refreshNextPlannifiedDate();
-  }
+refreshNextPlannifiedDate();
 
-  createDocumentation() {
-    const tomorow = LocalDateTime.now(ZoneOffset.UTC).plusDays(1);
-    return {
-      name: "Titre de votre communication",
-      content: "<h1>Partie 1</h1><p>Le corps de votre <b>communication</b>",
-      lakeIds: [],
-      isNational: false,
-      datePublicationDebut: [
-        tomorow.year(),
-        tomorow.monthValue(),
-        tomorow.dayOfMonth(),
-        7,
-        30
-      ],
-      datePublicationFin: [
-        tomorow.year(),
-        tomorow.monthValue(),
-        tomorow.dayOfMonth(),
-        7,
-        30
-      ],
-      isPublished: false
-    };
-  }
+function createDocumentation() {
+  const tomorow = LocalDateTime.now(ZoneOffset.UTC).plusDays(1);
+  return {
+    name: "Titre de votre communication",
+    content: "<h1>Partie 1</h1><p>Le corps de votre <b>communication</b>",
+    lakeIds: [],
+    isNational: false,
+    datePublicationDebut: [
+      tomorow.year(),
+      tomorow.monthValue(),
+      tomorow.dayOfMonth(),
+      7,
+      30
+    ],
+    datePublicationFin: [
+      tomorow.year(),
+      tomorow.monthValue(),
+      tomorow.dayOfMonth(),
+      7,
+      30
+    ],
+    isPublished: false
+  };
+}
 
-  computeIsPublicAndMiniatureURLs(actualites: any[]) {
-    actualites.forEach(actualite => {
-      let now = LocalDateTime.now(ZoneOffset.UTC);
-      let dateDebut = this.parseLocalDateTime(actualite.datePublicationDebut);
-      let dateFin = this.parseLocalDateTime(actualite.datePublicationFin);
+function computeIsPublicAndMiniatureURLs(actualites: any[]) {
+  actualites.forEach(actualite => {
+    const now = LocalDateTime.now(ZoneOffset.UTC);
+    const dateDebut = parseLocalDateTime(actualite.datePublicationDebut);
+    const dateFin = parseLocalDateTime(actualite.datePublicationFin);
 
-      actualite.isPublic =
-        now.isAfter(LocalDateTime.from(nativeJs(dateDebut))) &&
-        now.isBefore(LocalDateTime.from(nativeJs(dateFin)));
+    actualite.isPublic =
+      now.isAfter(LocalDateTime.from(nativeJs(dateDebut))) &&
+      now.isBefore(LocalDateTime.from(nativeJs(dateFin)));
 
-      actualite.miniatureURL = actualite.miniatureId
-        ? Constants.apiUrl("/v1/news-picture/" + actualite.miniatureId)
-        : "";
+    actualite.miniatureURL = actualite.miniatureId
+      ? Constants.apiUrl("/v1/news-picture/" + actualite.miniatureId)
+      : "";
 
-      actualite.lakeNames = actualite.isNational ?
-        "National" :
-        actualite.lakeIds.map((lakeId: string) => this.lakesIdToNameMap.get(lakeId)).join(", ");
-    });
-  }
+    actualite.lakeNames = actualite.isNational ?
+      "National" :
+      actualite.lakeIds.map((lakeId: string) => lakesIdToNameMap.get(lakeId)).join(", ");
+  });
+}
 
-  sendNotification(actualite: any) {
-    this.$buefy.dialog.confirm({
-      title: "Envoi de notification mail",
-      message: `Confirmez-vous souhaiter vouloir envoyer immédiatement un courriel aux utilisateurs de Fishola avec le contenu de l'actualité <i><b>${actualite["name"]}</b></i> ?`,
-      cancelText: "Annuler",
-      confirmText: "Envoyer notification immédiatement",
-      type: "is-success",
-      onConfirm: async () => {
-        try {
-          await BackendService.backendGet(
-            "/v1/news-notifications/send/" + actualite.id
-          );
-          this.$buefy.toast.open({
-            type: "is-success",
-            message: "Notification envoyée"
-          });
-          this.refreshNextPlannifiedDate();
-        } catch (e) {
-          this.$buefy.toast.open({
-            message:
-              "Une erreur est survenue lors de l'envoi de la notification",
-            type: "is-danger"
-          });
-        }
+function sendNotification(actualite: any) {
+  Dialog.confirm({
+    title: "Envoi de notification mail",
+    message: `Confirmez-vous souhaiter vouloir envoyer immédiatement un courriel aux utilisateurs de Fishola avec le contenu de l'actualité <i><b>${actualite["name"]}</b></i> ?`,
+    cancelText: "Annuler",
+    confirmText: "Envoyer notification immédiatement",
+    type: "is-success",
+    onConfirm: async () => {
+      try {
+        await BackendService.backendGet(
+          "/v1/news-notifications/send/" + actualite.id
+        );
+        Toast.open({
+          type: "is-success",
+          message: "Notification envoyée"
+        });
+        refreshNextPlannifiedDate();
+      } catch (e) {
+        Toast.open({
+          message:
+            "Une erreur est survenue lors de l'envoi de la notification",
+          type: "is-danger"
+        });
       }
-    });
-  }
-
-  parseLocalDateTime(someLocalDateTime: number[]): Date {
-    if (someLocalDateTime[5]) {
-      return new Date(
-        someLocalDateTime[0],
-        someLocalDateTime[1] - 1,
-        someLocalDateTime[2],
-        someLocalDateTime[3],
-        someLocalDateTime[4],
-        someLocalDateTime[5]
-      );
-    } else {
-      return new Date(
-        someLocalDateTime[0],
-        someLocalDateTime[1] - 1,
-        someLocalDateTime[2],
-        someLocalDateTime[3],
-        someLocalDateTime[4]
-      );
     }
+  });
+}
+
+function parseLocalDateTime(someLocalDateTime: number[]): Date {
+  if (someLocalDateTime[5]) {
+    return new Date(
+      someLocalDateTime[0],
+      someLocalDateTime[1] - 1,
+      someLocalDateTime[2],
+      someLocalDateTime[3],
+      someLocalDateTime[4],
+      someLocalDateTime[5]
+    );
+  } else {
+    return new Date(
+      someLocalDateTime[0],
+      someLocalDateTime[1] - 1,
+      someLocalDateTime[2],
+      someLocalDateTime[3],
+      someLocalDateTime[4]
+    );
   }
+}
 
-  async refreshNextPlannifiedDate(): Promise<void> {
-    try {
-      this.loggedAdmin = await BackendService.backendGet("/v1/admin/check");
-      let lakes = await BackendService.backendGet("/v1/referential/lakes");
-        lakes.forEach((lake: { id: string; name: string }) => {
-          this.lakesIdToNameMap.set(lake.id, lake.name);
-      });
-      lakes.forEach( (l: any) => {
-        this.lakesOptions.push({
-          id: l.id,
-          label: l.name,
-        })
-      });
+async function refreshNextPlannifiedDate(): Promise<void> {
+  try {
+    loggedAdmin.value = await BackendService.backendGet("/v1/admin/check");
+    const lakes = await BackendService.backendGet("/v1/referential/lakes");
+    lakes.forEach((lake: { id: string; name: string }) => {
+      lakesIdToNameMap.set(lake.id, lake.name);
+    });
+    lakes.forEach((l: any) => {
+      lakesOptions.push({
+        id: l.id,
+        label: l.name,
+      })
+    });
 
-      this.docColumns = [
+    docColumns.value = [
       {
         field: "id",
         label: "Identifiant technique",
@@ -206,7 +201,7 @@ export default class DocumentationVue extends Vue {
         label: "National (concerne tous les plans d'eau)",
         visible: false,
         showItemIfFunction: (news) => {
-          return this.loggedAdmin.isNationalAdmin;
+          return loggedAdmin.value.isNationalAdmin;
         },
       },
       {
@@ -220,7 +215,7 @@ export default class DocumentationVue extends Vue {
         possibleValuesForItemFunction: (news) => {
           return news.lakeIds ?? [];
         },
-        arrayOptions: this.lakesOptions
+        arrayOptions: lakesOptions
       },
       {
         field: "datePublicationFin",
@@ -247,15 +242,14 @@ export default class DocumentationVue extends Vue {
       }
     ];
 
-      const nextCheckDate= await BackendService.backendGet("/v1/news-notifications/next-check")
-      this.nextPlannifiedDate = nextCheckDate;
-    } catch (error) {
-      this.$buefy.toast.open({
-        message: "Vous n'êtes plus connecté\u00B7e",
-        type: "is-danger"
-      });
-      await router.push("/login");
-    }
+    const nextCheckDate = await BackendService.backendGet("/v1/news-notifications/next-check")
+    nextPlannifiedDate.value = nextCheckDate;
+  } catch (error) {
+    Toast.open({
+      message: "Vous n'êtes plus connecté\u00B7e",
+      type: "is-danger"
+    });
+    await router.push("/login");
   }
 }
 </script>
