b90a6567277be7e6caf26d6832fd943e0c012976
lcazef <caze@codelutin.com>
2026-06-03 11:33:13 +0200

    chore(android): migrate android/build.gradle to kotlin dsl

--- fishola-mobile/android/build.gradle.kts
+++ fishola-mobile/android/build.gradle.kts
@@ -7,15 +7,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.13.2'
-        classpath 'com.google.gms:google-services:4.4.4'
+        classpath("com.android.tools.build:gradle:8.13.2")
+        classpath("com.google.gms:google-services:4.4.4")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-apply from: "variables.gradle"
+apply(from = "variables.gradle")
 
 allprojects {
     repositories {
@@ -24,7 +24,7 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
 
